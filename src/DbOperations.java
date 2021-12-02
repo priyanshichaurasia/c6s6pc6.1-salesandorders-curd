2111ab8846f23d1132246aa35da91c52e8e48ec9
rahulc-sfdx <rahulc2345@gmail.com>
2021-12-02 18:25:13 +0530

    prgrm update

--- src/DbOperations.java
+++ src/DbOperations.java
@@ -1,6 +1,6 @@
 import java.sql.*;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 public class DbOperations {
 
@@ -35,7 +35,6 @@ public class DbOperations {
                     "root", "root");
             String query = "insert into salesperson values(?,?,?,?)";
             PreparedStatement ps = con.prepareStatement(query);
-            // id, name, city, rate
             ps.setInt(1, sp.getSalesId());
             ps.setString(2,sp.getFirstName());
             ps.setString(3,sp.getCity());
@@ -54,7 +53,7 @@ public class DbOperations {
         return result;
     }
 
-    public boolean removeSalesperson(String firstName){
+    public boolean removeSalesperson(int salesId){
         boolean result = true;
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -62,7 +61,7 @@ public class DbOperations {
                     "root", "root");
             String query = "delete from salesperson where salesId = ?";
             PreparedStatement ps = con.prepareStatement(query);
-            ps.setString(1,firstName);
+            ps.setInt(1,salesId);
             int res = ps.executeUpdate();
             if(res==1){
                 result = true;
@@ -75,4 +74,102 @@ public class DbOperations {
         }
         return result;
     }
+
+    public boolean addCustomer(Customer c){
+        boolean result = true;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/CarsSalesData",
+                    "root", "root");
+            String query = "insert into customer values(?,?,?,?,?,?)";
+            PreparedStatement ps = con.prepareStatement(query);
+            ps.setInt(1,c.getCustomerId());
+            ps.setString(2,c.getName());
+            ps.setString(3, c.getLastName());
+            ps.setString(4,c.getCity());
+            ps.setInt(5,c.getRating());
+            ps.setInt(6,c.getSalesId());
+
+            int res = ps.executeUpdate();
+            if(res==1){
+                result = true;
+            }
+            ps.close();
+            con.close();
+        }
+        catch (Exception ex){
+            System.out.println(ex);
+        }
+        return result;
+    }
+
+    public boolean addOrders(Orders or){
+        boolean result = true;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/CarsSalesData",
+                    "root", "root");
+            String query = "insert into orders values(?,?,?,?)";
+            PreparedStatement ps = con.prepareStatement(query);
+            ps.setInt(1, or.getOrderId());
+            ps.setInt(2, or.getAmount());
+            ps.setDate(3, new java.sql.Date(new java.util.Date().getTime()));
+            ps.setInt(4, or.getCustomerId());
+            int res = ps.executeUpdate();
+            if(res==1){
+                result = true;
+            }
+            ps.close();
+            con.close();
+        }
+        catch (Exception ex){
+            System.out.println(ex);
+        }
+        return result;
+    }
+       public Customer getCustomerByName(String name){
+           Customer cust = null;
+           try {
+               Class.forName("com.mysql.cj.jdbc.Driver");
+               Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/CarsSalesData", "root",
+                       "root");
+               String query = "insert into customer values where name = ?";
+               PreparedStatement ps = con.prepareStatement(query);
+               ps.setString(1,name);
+               ResultSet rs = ps.executeQuery();
+               if(rs.next()){
+                   cust = new Customer(rs.getInt(1), rs.getString(2), rs.getString(3),
+                           rs.getString(4), rs.getInt(5), rs.getInt(6));
+               }
+               ps.close();
+               con.close();
+           }
+           catch (Exception ex){
+               System.out.println(ex);
+           }
+           return cust;
+       }
+
+       public boolean update(Orders or) {
+           boolean result = false;
+           try {
+               Class.forName("com.mysql.cj.jdbc.Driver");
+               Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/CarsSalesData", "root",
+                       "root");
+               String query = "update orders set orderId = ?, amount = ?, orderDate = ?, where customerId = ?";
+               PreparedStatement ps = con.prepareStatement(query);
+               ps.setInt(1, or.getOrderId());
+               ps.setInt(2, or.getAmount());
+               ps.setDate(3, new java.sql.Date(new java.util.Date().getTime()));
+               int res = ps.executeUpdate();
+               if (res != 0) {
+                   result = true;
+               }
+               ps.close();
+               con.close();
+           } catch (Exception ex) {
+               System.out.println(ex);
+           }
+           return result;
+       }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
 public class Main {
-    public  static  void  main(String [] args){
+    public  static  void  main(String [] args) throws ParseException {
         DbOperations dbo = new DbOperations();
         List<Salesperson> salesperson = dbo.getAllSalesperson();
         for(Salesperson sp : salesperson){
@@ -11,7 +13,30 @@ public class Main {
         boolean result = dbo.addSalesperson(sp);
         System.out.println("\n new salesperson record inserted " +result);
 
-        boolean remove = dbo.removeSalesperson("axelrod");
+        boolean remove = dbo.removeSalesperson(1003);
         System.out.println("\n Salesperson is removed " +remove);
+
+        Customer cust = new Customer(12563,"Samuel", "Thomas", "Stockholm",
+                150,1111);
+        boolean addcust = dbo.addCustomer(cust);
+        System.out.println("\nCustomer Added " +addcust);
+        java.util.Date dt = new SimpleDateFormat("yyyy-mm-dd").parse("2020-05-15");
+        Orders ord2 = new Orders(12365, 3000, dt, cust.getCustomerId());
+        boolean addorder = dbo.addOrders(ord2);
+
+        Customer cust1 = dbo.getCustomerByName("Samuel");
+        if(cust1==null) {
+            System.out.println("Customer found ");
+        }
+        else{
+            ord2.setAmount(3500);
+            boolean result1 = dbo.update(ord2);
+                if(result1==true){
+                    System.out.println("updated");
+                }
+                else{
+                    System.out.println("Not updated");
+                }
+            }
     }
 }
